893c5320af238f1957ad0763deb69fc63d767271
Raj <rajesh.prasad50@gmail.com>
2020-12-14 02:46:07 +0530

    Initial commit Role-Based Spring Security

---
+++ src/main/java/com/security/group/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.security.group.repository;
+
+import com.security.group.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUserName(String username);
+}
+
---
+++ src/main/java/com/security/group/service/GroupUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.security.group.service;
+
+
+import com.security.group.entity.User;
+import com.security.group.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+public class GroupUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = repository.findByUserName(username);
+        return user.map(GroupUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " Does not exist in the system"));
+    }
+}
+
